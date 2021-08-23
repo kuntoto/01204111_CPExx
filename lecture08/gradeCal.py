6d1d7b6ece8ddb773c6138211932044d1a28598e
KunToto Na MikeLab Dot Net <86561931+kuntoto@users.noreply.github.com>
2021-08-23 13:06:40 +0700

    Create gradeCal.py
    Demo code for Problem Solving using List and Dictionary in Python, 111@MikeLab.Net/KU

---
+++ lecture08/gradeCal.py
@@ -0,0 +1,67 @@
+def getSubGrade():
+  subGrade = {}
+  while True:
+    printSubjMenu()
+    try:
+      n = int(input('Select either subject [1]..[8] or 0: '))
+      if n==0:
+        break
+      if n not in range(1,9):
+        raise ValueError('')
+    except ValueError as e:
+      print(f' ERROR: only interger 1..8 accepted!')
+      continue
+    g = input(f'Enter grade you get for [{subList[n-1]}]: ').upper()
+    if g not in grade.keys():
+      print(f' ERROR: either of A,B+,B,..F is accepted!')
+      continue
+    subGrade[subList[n-1]] = g 
+    print(subGrade)
+  return subGrade
+
+def printSubjMenu():
+  lenSubList, s, ss = len(subList), [], ''
+  for i in range(lenSubList):
+    ss = f'[{i+1}] {subList[i]}'
+    s.append(f'{ss:<25}')
+    if len(s)%2==0:
+      print(s[0],s[1])
+      s = []
+
+def calGrade(v):
+  lenV, C, myS = len(v), 0, 0 
+  for s,g in v.items():
+    C, myS = C + subDic[s], myS + subDic[s] * grade[g] 
+    print(f'{s} ({g})', end='')
+    lenV -= 1 
+    if lenV==0:
+      print()
+    else:
+      print(', ', end='')
+  print(f' GPA: {myS/C:.2f}')
+    
+def printGrades(studDic):
+  for n,v in studDic.items():
+    print(f'Name: {n}')
+    calGrade(v)
+
+def gradeCal():
+  studDic = {}
+  while True:
+    name = input('Enter student\'s name or press ENTER to end: ')
+    if name == '':
+      printGrades(studDic)
+      break
+    subjGrade = getSubGrade()
+    print(name, subjGrade)
+    studDic[name] = subjGrade 
+  return studDic
+
+## main begins here
+subDic = {'Physic I': 3, 'Lab Physic I': 1, 'Math I': 3, 'Com Programming': 3, 'Thai Lang Com': 3, 'Art of Living': 3, 'Land of Smile': 3, 'Intro Japanese': 3}
+subList = ['Physic I', 'Lab Physic I', 'Math I', 'Com Programming', 'Thai Lang Com', 'Art of Living', 'Land of Smile', 'Intro Japanese']
+grade = {'A':4,'B+':3.5,'B':3,'C+':2.5,'C':2,'D+':1.5,'D':1,'F':0}
+
+#studDic = {'Kun Toto': {'Physic I': 'A', 'Lab Physic I': 'C+', 'Thai Lang Com': 'B+', 'Land of Smile': 'D', 'Intro Japanese': 'B+'}, 'Somchai Rukdee': {'Lab Physic I': 'B+', 'Physic I': 'B', 'Math I': 'C', 'Com Programming': 'D', 'Thai Lang Com': 'F', 'Art of Living': 'A', 'Land of Smile': 'A'}}
+
+studDic = gradeCal()
